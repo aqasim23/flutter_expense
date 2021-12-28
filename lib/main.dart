b30640bd0548f4212131a8ce6fcb321b44ada070
aqasim23 <57294858+aqasim23@users.noreply.github.com>
2021-12-28 20:40:20 +0500

    expense app

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:expense/Widgets/transaction_list.dart';
+import 'package:expense/Widgets/user_transactions.dart';
+import 'package:flutter/material.dart';
+import './Widgets/user_transactions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense',
+      home: myHomePage(),
+    );
+  }
+}
+
+class myHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions(),
+          ],
+        ),
+      ),
+    );
+  }
+}
