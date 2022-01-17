7dce4674ef492343a320e5555e62574873444c62
aqasim23 <57294858+aqasim23@users.noreply.github.com>
2022-01-18 04:35:39 +0500

    module 102 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:expense/Widgets/transaction_list.dart';
+import './Widgets/transaction_list.dart';
 import 'package:expense/Widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
+import './Widgets/new_transaction.dart';
 import './Widgets/user_transactions.dart';
+import './Models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,21 +14,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Expense',
       home: myHomePage(),
     );
   }
 }
 
-class myHomePage extends StatelessWidget {
+class myHomePage extends StatefulWidget {
+  @override
+  State<myHomePage> createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
   // String titleInput;
-  // String amountInput;
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 99.01,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Grocery',
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (bCtx) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense App'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -41,9 +95,16 @@ class myHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
